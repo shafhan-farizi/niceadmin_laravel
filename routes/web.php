02619a698e74a70c55e26769bc3c2a39e50bfdd2
shafhan <shafhan.fa@gmail.com>
2024-06-11 20:59:13 +0700

    tugas pertemuan 13 | landing page

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\StudentController;
 
 //route route
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
 
@@ -26,23 +26,23 @@ Route::get('/', function () {
 
 Route::middleware('auth')->group(function () {
     // dashboard
-Route::get('admin/dashboard',[DashboardController::class,'index'])->name('dashboard');
+    Route::get('admin/dashboard',[DashboardController::class,'index'])->name('dashboard');
 
-// student
-Route::get('admin/student', [StudentController::class, 'index'])->name('student.index');
-Route::get('admin/student/create', [StudentController::class, 'create'])->name('student.create');
-Route::post('admin/student/create', [StudentController::class, 'store'])->name('student.store');
-Route::get('admin/student/edit/{id}', [StudentController::class, 'edit'])->name('student.edit');
-Route::put('admin/student/edit/{id}', [StudentController::class, 'update'])->name('student.update');
-Route::delete('admin/student/delete/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
+    // student
+    Route::get('admin/student', [StudentController::class, 'index'])->name('student.index');
+    Route::get('admin/student/create', [StudentController::class, 'create'])->name('student.create');
+    Route::post('admin/student/create', [StudentController::class, 'store'])->name('student.store');
+    Route::get('admin/student/edit/{id}', [StudentController::class, 'edit'])->name('student.edit');
+    Route::put('admin/student/edit/{id}', [StudentController::class, 'update'])->name('student.update');
+    Route::delete('admin/student/delete/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
 
-// course
-Route::get('admin/course', [CourseController::class, 'index'])->name('course.index');
-Route::get('admin/course/create', [CourseController::class, 'create'])->name('course.create');
-Route::post('admin/course/create', [CourseController::class, 'store'])->name('course.store');
-Route::get('admin/course/edit/{id}', [CourseController::class, 'edit'])->name('course.edit');
-Route::put('admin/course/edit/{id}', [CourseController::class, 'update'])->name('course.update');
-Route::delete('admin/course/delete/{id}', [CourseController::class, 'destroy'])->name('course.destroy');
+    // course
+    Route::get('admin/course', [CourseController::class, 'index'])->name('course.index');
+    Route::get('admin/course/create', [CourseController::class, 'create'])->name('course.create');
+    Route::post('admin/course/create', [CourseController::class, 'store'])->name('course.store');
+    Route::get('admin/course/edit/{id}', [CourseController::class, 'edit'])->name('course.edit');
+    Route::put('admin/course/edit/{id}', [CourseController::class, 'update'])->name('course.update');
+    Route::delete('admin/course/delete/{id}', [CourseController::class, 'destroy'])->name('course.destroy');
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
